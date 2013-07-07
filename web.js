1f1ec14ba0d023c3f99eb759cb0612ebf2a80de3
Cristian PL <cpeinado@gmail.com>
2013-07-07 20:38:18 +0200

    Modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require ('fs');
+
+var Buff = new Buffer('');
+
+Buff = fs.readFileSync("index.html");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello Isi y N3!');
+  //response.send('Hello Isi y N3!');
+    response.send(Buff.toString());
 });
 
 var port = process.env.PORT || 5000;
